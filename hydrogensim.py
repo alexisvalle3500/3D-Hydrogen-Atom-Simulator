6de5a3cdb2ff3df6b01ad62105a34800311656ee
alexisvalle3500 <alexisvalle3500@gmail.com>
2026-04-21 16:55:03 -0700

    add opengl dark background and version hints please use py 3.11

--- hydrogensim.py
+++ hydrogensim.py
@@ -1,13 +1,23 @@
 import glfw
+from OpenGL.GL import *
 
 WIDTH, HEIGHT = 1440, 1080
 
 def main():
     glfw.init()
+
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+
     window = glfw.create_window(WIDTH, HEIGHT, "hydrogen atom simulator", None, None)
     glfw.make_context_current(window)
 
     while not glfw.window_should_close(window):
+        glClearColor(0.03, 0.03, 0.07, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
         glfw.swap_buffers(window)
         glfw.poll_events()
 
